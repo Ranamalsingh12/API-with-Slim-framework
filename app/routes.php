e96fb7192ffc311a8e3239214c472b34f25e5c1f
Ranamalsingh12 <ranams99911@gmail.com>
2023-03-02 23:05:55 +0530

    adding simple get api

--- app/routes.php
+++ app/routes.php
@@ -10,19 +10,24 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $app->options('/{routes:.*}', function (Request $request, Response $response) {
-        // CORS Pre-Flight OPTIONS Request Handler
-        return $response;
-    });
+    // $app->options('/{routes:.*}', function (Request $request, Response $response) {
+    //     // CORS Pre-Flight OPTIONS Request Handler
+    //     return $response;
+    // });
 
-    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-        $name = $args['name'];
-        $response->getBody()->write('Hello world!' .  $name);
-        return $response;
-    });
+    // $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    //     $name = $args['name'];
+    //     $response->getBody()->write('Hello world!' .  $name);
+    //     return $response;
+    // });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
+    // $app->group('/users', function (Group $group) {
+    //     $group->get('', ListUsersAction::class);
+    //     $group->get('/{id}', ViewUserAction::class);
+    // });
+
+    $app->get('/hello', function (Request $request, Response $response, array $args) {
+        $response->getBody()->write('Hello I am using the slim framework');
+        return $response;
     });
 };
